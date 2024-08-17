92d658176f3bc40cd66a38e5bf21f4c034867a3a
swakwork <swakwork+git@gmail.com>
2024-08-17 20:34:29 +0530

    feat(Twitter): Custom deeplinks

--- app/src/main/java/app/revanced/integrations/twitter/settings/ActivityHook.java
+++ app/src/main/java/app/revanced/integrations/twitter/settings/ActivityHook.java
@@ -3,9 +3,11 @@ package app.revanced.integrations.twitter.settings;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import androidx.appcompat.widget.Toolbar;
 import app.revanced.integrations.shared.Utils;
+import android.app.Fragment;
+import app.revanced.integrations.twitter.settings.featureflags.FeatureFlagsFragment;
 
 import static app.revanced.integrations.shared.Utils.context;
 
@@ -13,31 +15,46 @@ import static app.revanced.integrations.shared.Utils.context;
 public class ActivityHook {
     public static Toolbar toolbar;
     private static final String EXTRA_PIKO_SETTINGS = "piko_settings";
+    private static final String EXTRA_FEATURE_FLAGS = "piko_feature_flags";
 
     public static boolean create(Activity act) {
-        if (!act.getIntent().getBooleanExtra(EXTRA_PIKO_SETTINGS, false)) {
-            return false;
+        Fragment fragment = null;
+        if (act.getIntent().getBooleanExtra(EXTRA_PIKO_SETTINGS, false)) {
+            fragment = new SettingsFragment();
+        }else if (act.getIntent().getBooleanExtra(EXTRA_FEATURE_FLAGS, false)) {
+            fragment = new FeatureFlagsFragment();
         }
 
-        Log.d("BRUH", "Activity requested");
+        if(fragment!=null) {
+           // Log.d("BRUH", "Activity requested");
 
-        act.setContentView(Utils.getResourceIdentifier("preference_fragment_activity", "layout"));
-        toolbar = act.findViewById(Utils.getResourceIdentifier("toolbar", "id"));
-        toolbar.setNavigationIcon(Utils.getResourceIdentifier("ic_vector_arrow_left", "drawable"));
-        toolbar.setTitle(Utils.getResourceString("piko_title_settings"));
-        toolbar.setNavigationOnClickListener(view -> act.onBackPressed());
+            act.setContentView(Utils.getResourceIdentifier("preference_fragment_activity", "layout"));
+            toolbar = act.findViewById(Utils.getResourceIdentifier("toolbar", "id"));
+            toolbar.setNavigationIcon(Utils.getResourceIdentifier("ic_vector_arrow_left", "drawable"));
+            toolbar.setTitle(Utils.getResourceString("piko_title_settings"));
+            toolbar.setNavigationOnClickListener(view -> act.onBackPressed());
 
-        act.getFragmentManager().beginTransaction().replace(Utils.getResourceIdentifier("fragment_container", "id"), new SettingsFragment()).commit();
+            act.getFragmentManager().beginTransaction().replace(Utils.getResourceIdentifier("fragment_container", "id"), fragment).commit();
 
-        return true;
+            return true;
+        }
+        return false;
     }
 
-    public static void startSettingsActivity() throws Exception {
+    private static void startActivity(String activity_name)throws Exception{
         Intent intent = new Intent(context, Class.forName("com.twitter.android.AuthorizeAppActivity"));
         Bundle bundle = new Bundle();
-        bundle.putBoolean(EXTRA_PIKO_SETTINGS, true);
+        bundle.putBoolean(activity_name, true);
         intent.putExtras(bundle);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+    }
+
+    public static void startSettingsActivity() throws Exception {
+        startActivity(EXTRA_PIKO_SETTINGS);
+    }
+
+    public static void startFeatureFlagsActivity() throws Exception {
+        startActivity(EXTRA_FEATURE_FLAGS);
     }
 }
---
+++ app/src/main/java/app/revanced/integrations/twitter/settings/DeepLink.java
@@ -0,0 +1,41 @@
+package app.revanced.integrations.twitter.settings;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.List;
+import android.net.Uri;
+import  app.revanced.integrations.twitter.Utils;
+
+@SuppressWarnings("deprecation")
+public class DeepLink {
+
+    public static boolean deeplink(Activity act) {
+        try {
+            Uri deeplink = act.getIntent().getData();
+            List<String> deeplinkSegments = deeplink.getPathSegments();
+            int segmentSize = deeplinkSegments.size();
+
+            if(deeplinkSegments.size() < 2) return false;
+
+            if(!(deeplinkSegments.get(0).toLowerCase().equals("i"))) return false;
+
+            String mainPath = deeplinkSegments.get(1).toLowerCase();
+            boolean isPiko = mainPath.equals("piko") || mainPath.equals("pikosettings");
+
+            if(segmentSize == 2 && isPiko){
+                ActivityHook.startSettingsActivity();
+                return true;
+            }else if(mainPath.equals("flags")){
+                ActivityHook.startFeatureFlagsActivity();
+                return true;
+            }
+        }catch(Exception e){
+            Utils.logger(e);
+        }
+
+        return false;
+    }
+
+
+}
