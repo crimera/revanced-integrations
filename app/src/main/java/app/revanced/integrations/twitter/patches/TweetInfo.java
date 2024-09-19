e61241a2034e893d9d84d794d999baae1f6e29e6
swakwork <swakwork+git@gmail.com>
2024-09-19 11:22:26 +0530

    fix(Twitter): fix Tweet Info related patches in/from 10.58

--- app/src/main/java/app/revanced/integrations/twitter/patches/TweetInfo.java
+++ app/src/main/java/app/revanced/integrations/twitter/patches/TweetInfo.java
@@ -2,31 +2,59 @@ package app.revanced.integrations.twitter.patches;
 
 
 import com.twitter.api.model.json.core.JsonApiTweet;
+import com.twitter.model.json.core.JsonTweetQuickPromoteEligibility;
 import app.revanced.integrations.twitter.Pref;
 import app.revanced.integrations.twitter.settings.SettingsStatus;
 import java.lang.reflect.Field;
 
 public class TweetInfo {
     private static final boolean hideCommNotes,hidePromoteBtn,forceTranslate;
+    private static Field commNotesField,promoteBtnField,translateField;
+
     static {
-        hideCommNotes = (Pref.hideCommNotes() && SettingsStatus.hideCommunityNote);;
-        hidePromoteBtn = (Pref.hidePromoteBtn() && SettingsStatus.hidePromoteButton);;
-        forceTranslate = (Pref.enableForceTranslate() && SettingsStatus.forceTranslate);;
+        hideCommNotes = (Pref.hideCommNotes() && SettingsStatus.hideCommunityNote);
+        hidePromoteBtn = (Pref.hidePromoteBtn() && SettingsStatus.hidePromoteButton);
+        forceTranslate = (Pref.enableForceTranslate() && SettingsStatus.forceTranslate);
+        commNotesField = null;
+        promoteBtnField = null;
+        translateField = null;
+
     }
 
+    private static void loader(Class jsonApiTweetCls){
+        commNotesField = null;
+        promoteBtnField = null;
+        translateField = null;
+
+        Field[] fields = jsonApiTweetCls.getDeclaredFields();
+        for(Field field : fields){
+            if (field.getType() == boolean.class) {
+                if(commNotesField == null){
+                    commNotesField = field;
+                    continue;
+                }
+                translateField = field;
+                continue;
+            }
+            if (field.getType() == JsonTweetQuickPromoteEligibility.class) {
+                promoteBtnField = field;
+            }
+        }
+    }
     public static JsonApiTweet checkEntry(JsonApiTweet jsonApiTweet) {
         try {
             Class jsonApiTweetCls = jsonApiTweet.getClass();
+            if( commNotesField == null || promoteBtnField == null || commNotesField == null ){
+                loader(jsonApiTweetCls);
+            }
             if(hideCommNotes){
-                Field f = jsonApiTweetCls.getDeclaredField("e0");
-                f.set(jsonApiTweet,null);
+                commNotesField.set(jsonApiTweet,null);
             }
             if(hidePromoteBtn){
-                Field f = jsonApiTweetCls.getDeclaredField("q0");
-                f.set(jsonApiTweet,null);
+                promoteBtnField.set(jsonApiTweet,null);
             }
             if(forceTranslate){
-                jsonApiTweet.r0 = true;
+                translateField.set(jsonApiTweet,true);
             }
 
         } catch (Exception unused) {
