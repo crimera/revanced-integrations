1c76b7d7635bdc1c7045154b601e6922e45e18e3
swakwork <swakwork+git@gmail.com>
2025-06-21 22:00:12 +0530

    feat(Twitter): Added `Hide community badges` patch

--- app/src/main/java/app/revanced/integrations/twitter/settings/Settings.java
+++ app/src/main/java/app/revanced/integrations/twitter/settings/Settings.java
@@ -64,6 +64,7 @@ public class Settings extends BaseSettings {
     public static final BooleanSetting TIMELINE_ENABLE_VID_FORCE_HD = new BooleanSetting("timeline_enable_vid_force_hd", true);
     public static final BooleanSetting TIMELINE_HIDE_NUDGE_BUTTON = new BooleanSetting("timeline_hide_nudge_button", false);
     public static final BooleanSetting TIMELINE_SHOW_SENSITIVE_MEDIA = new BooleanSetting("timeline_show_sensitive_media", true);
+    public static final BooleanSetting TIMELINE_HIDE_COMM_BADGE = new BooleanSetting("timeline_hide_community_badge", false);
 
     public static final BooleanSetting PREMIUM_READER_MODE = new BooleanSetting("premium_reader_mode", false);
     public static final BooleanSetting PREMIUM_UNDO_POSTS = new BooleanSetting("premium_undo_posts", false);
